5f8971729682496ed5a345e5c8bd8da27051d5ba
kohicha <yensyd.francisco@gmail.com>
2024-04-18 22:10:12 +0800

    Add project files.

---
+++ lmsmaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace lmsmaui
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ lmsmaui/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace lmsmaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnEntryTextChanged(object sender,TextChangedEventArgs e)
+        {
+            if(MsgTxt != null)
+            {
+                DisplayLbl.Text = MsgTxt.Text.ToString();
+            }           
+        }
+    }
+
+}
